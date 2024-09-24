4343953b21056ba70aa4a15db994f48718406bec
WilliamH <91361904+hujingwen1025@users.noreply.github.com>
2024-09-24 19:55:35 +0800

    Added partial checkpin verification
    Added verification code to the code with hash verification and sendjson

--- keyshare/keyshareserver.py
+++ keyshare/keyshareserver.py
@@ -1,6 +1,40 @@
 from flask import *
+from datetime import date
+import requests
+import hashlib
 
 app = Flask(__name__)
+
+pincheckurl = "https://example.org"
+unitsecretpath = "./unitsecret.txt"
+
+def read_file_to_string(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
+
+def post_json_request(url, json_data=None):
+    session = requests.Session()
+    
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    try:
+        response = session.post(url, headers=headers, json=json_data)
+        
+        response.raise_for_status()
+        
+        return response.json()
+    
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+    finally:
+        session.close()
+
+unitsecret = read_file_to_string(unitsecretpath)
 
 @app.route('/')
 def noresponse():
@@ -9,6 +43,20 @@ def noresponse():
 @app.route('/keypad.html')
 def keypadrender():
     return render_template('keypad.html')
+
+@app.route('/checkpin', methods=['POST'])
+def apiresponding():
+    pin = request.json.get('pin')
+    date = str(date.today())
+    hashtext = date + unitsecret + pin
+    verihash = hashlib.sha256(hashtext.encode('utf-8')).hexdigest()
+    sendjson = {
+        "hash": verihash,
+        "pin": pin
+    }
+    response = post_json_request(pincheckurl, sendjson)
+    #Add response handle
+    return 0
 
 if __name__ == '__main__':
     app.run(host='localhost')
